de26e8fbbe95ec59f33f7fa98f93f785f59f8706
Nikhil chopra <nikhil0080012005@gmail.com>
2025-12-27 13:02:28 +0530

    added 2 backend route

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,90 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors"
+import { prismaClient } from "db";
+import { authMiddleware } from "./middleware";
+import { SignupSchema } from "common/inputs"
+import jwt from "jsonwebtoken"
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+
+app.post("/signin", async (req, res) => {
+    const {success, data} = SignupSchema.safeParse(req.body);
+    if(!success){
+        res.status(403).json({
+            message : "incorrect credentials"
+        })
+        return;
+    }
+
+    const email = data.email;
+    const password = data.password;
+
+    const user = await prismaClient.user.findFirst({
+        where:{
+            email
+        }
+    });
+
+    if(!user){
+        res.status(403).json({
+            message : "User not found",
+        })
+        return;
+    }
+
+    if(user.password != password){
+        res.status(403).json({
+            message : "incorrect",
+        })
+        return;
+    }
+
+    const token = jwt.sign({
+        userId: user.id
+    }, process.env.JWT_SECRET!);
+    
+    res.json({
+        token
+    })
+
+})
+
+app.get("/calender/:courseId", authMiddleware, async (req, res) => {
+    const courseId = req.params.courseId;
+    const course = await prismaClient.course.findFirst({
+        where :{
+            id : courseId
+        }
+    })
+
+    const purchase = await prismaClient.purchases.findFirst({
+        where: {
+            userId: req.userId,
+            courseId : courseId
+        }
+    })
+
+    if(!purchase){
+        res.status(411).json({
+            message : "you dont have access to course"
+        })
+        return;
+    }
+
+    if(!course){
+        res.status(411).json({
+            message : "course with id not found"
+        })
+
+        return;
+    }
+
+    res.json({
+        id : course?.id,
+        calenderId : course.calendarNotionId
+    })
+})
+
+app.listen(process.env.PORT || 3000);
---
+++ apps/backend/middleware.ts
@@ -0,0 +1,26 @@
+import type {NextFunction, Request, Response} from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+	const header = req.headers.authorization;
+    const token = header?.split(" ")?.[1];
+
+    if(!token){
+        return res.status(403).json({
+            message: "Missing token"
+        })
+    }
+
+    try{
+        const { userId } = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+        if(userId){
+            req.userId = userId;
+        }
+        next();
+    }
+    catch(e){
+        return res.status(403).json({
+            message: "token malformed"
+        })
+    }
+} 
