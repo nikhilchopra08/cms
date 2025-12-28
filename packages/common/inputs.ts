e0374cf145e68e450f7912ea83c64fafca447ac1
Nikhil chopra <nikhil0080012005@gmail.com>
2025-12-28 12:54:06 +0530

    Added the CreateUserSchema, migrated the db

--- packages/common/inputs.ts
+++ packages/common/inputs.ts
@@ -4,3 +4,9 @@ export const SignupSchema = z.object({
     email : z.string(),
     password : z.string()
 }); 
+
+export const CreateUserSchema = z.object({
+    email : z.string(),
+    password : z.string(),
+    number : z.string()
+})
