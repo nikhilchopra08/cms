bec81a1a659cd71d963a73c76f03a945ea795909
Nikhil chopra <nikhil0080012005@gmail.com>
2025-12-28 12:55:26 +0530

    WIP: create-user route

--- apps/backend/routes/admin.ts
+++ apps/backend/routes/admin.ts
@@ -1,13 +1,19 @@
-import { SignupSchema } from "common/inputs";
+import { CreateUserSchema, SignupSchema } from "common/inputs";
 import { prismaClient } from "db/client";
 import { Router } from "express";
 import { adminAuthMiddleware } from "../middleware";
 import jwt from "jsonwebtoken";
+import axios from "axios";
+
+const MPC_SERVER = [
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "http://localhost:3003",
+];
 
 const router = Router();
 
 export default router;
-
 
 router.post("/signin", async (req, res) => {
     console.log("inside the signin")
@@ -52,3 +58,32 @@ router.post("/signin", async (req, res) => {
 
 })
 
+router.post("/create-user", adminAuthMiddleware, async (req, res) => {
+    const {success, data} = CreateUserSchema.safeParse(req.body);
+    if(!success){
+        res.status(403).json({
+            message : "you are not admin"
+        })
+        return;
+    }
+
+    const user = await prismaClient.user.create({
+        data: {
+            email : data.email,
+            phone : data.number,
+            password: data.password,
+            role : "USER"
+        }
+    })
+
+    const responses = await Promise.all(MPC_SERVER.map(async (server) => {
+        const response = await axios.post(`${server}/create-user`, {
+            userId : user.id
+        })
+    }))
+
+    res.json({
+        message : "User Created",
+        user
+    })
+})
