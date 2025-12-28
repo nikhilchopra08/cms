23eff2ddc0e976dd2b1e06125cd539d0d546f8d8
Nikhil chopra <nikhil0080012005@gmail.com>
2025-12-28 13:16:58 +0530

    WIP mpc backend.

--- apps/mpc-backend/index.ts
+++ apps/mpc-backend/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express from "express"
+import { TSSCli } from 'solana-mpc-tss-lib/mpc';
+
+const cli = new TSSCli('devnet');
+
+const app = express();
+
+app.post("create-user", async(req, res) => {
+    const {userId} = req.body;
+    const participant = await cli.generate();
+
+    res.json({
+        publicKey : participant.publicKey
+    })
+
+})
+
+app.listen(3001);
